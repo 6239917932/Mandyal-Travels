33f4e56e7f063c67cabbdccccb864d4927b5dcf2
6239917932 <jasveer_singh@yahoo.com>
2026-08-10 14:08:56 +0530

    Clarify bus fare before promotions

--- app/buses/booking/payment/page.tsx
+++ app/buses/booking/payment/page.tsx
@@ -85,7 +85,7 @@ export default async function BusPaymentPage({
                 <dd>{seats.join(', ')}</dd>
               </div>
               <div className="flight-booking-page__total">
-                <dt>Total</dt>
+                <dt>Fare before offers</dt>
                 <dd>{money(offer.totalPrice)}</dd>
               </div>
             </dl>
