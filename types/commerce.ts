9fc02ae13c646f80344032be605ea5e52e4411ce
6239917932 <jasveer_singh@yahoo.com>
2026-08-10 13:43:22 +0530

    Allow validated hotel promotion requests

--- types/commerce.ts
+++ types/commerce.ts
@@ -142,6 +142,7 @@ export interface CreateHotelBookingRequest {
   availabilityLockId: string;
   guest: HotelBookingRecord['guest'];
   hotelSlug: string;
+  promotionCode?: string;
   quoteId: string;
 }
 
